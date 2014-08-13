72777e6c646611ec692398ce6e3fb47e142ccbbc
tdouglas <tdoug19@gmail.com>
2014-08-13 15:57:19 -0600

    Able to read in the original sample file and then massage that original signal.  At the point no filtering is done.  However, that signal is then massaged back in order to be analyzed.

--- RadiationFiltering/RadiationFiltering/RadiationFiltering.cpp
+++ RadiationFiltering/RadiationFiltering/RadiationFiltering.cpp
@@ -9,15 +9,20 @@
 
 #define SAMPLES   65536
 
-uint16_t input[SAMPLES];
-double floatInput[SAMPLES];
+//This is the max array we will need to read in for now.  We have 4 channels with 65536 
+//samples.  Change this if we are passing in more channels.
+uint16_t allChannelInput[SAMPLES * 4];
+uint16_t sampleInput[SAMPLES];
+double doubleInput[SAMPLES];
 
 uint16_t output[SAMPLES];
-double floatOutput[SAMPLES];
+double doubleOutput[SAMPLES];
 
 
-
-void intToFloat(uint16_t *input, double *output, int length)
+/** 
+* Convert an integer array to a double array
+*/
+void intToDouble(uint16_t *input, double *output, int length)
 {
 	int i;
 
@@ -26,39 +31,75 @@ void intToFloat(uint16_t *input, double *output, int length)
 	}
 }
 
-void floatToInt(double *input, uint16_t *output, int length)
-{
-	int i;
 
-	for (i = 0; i < length; i++) {
-		//This stuff is making sure the data is not out of range.
-//		if (input[i] > 32767.0) {
-//			input[i] = 32767.0;
-//		}
-//		else if (input[i] < -32768.0) {
-//			input[i] = -32768.0;
-//		}
-		// convert
-		output[i] = (int16_t)input[i];
+/*
+* Parse the input array and try to get the samples that we need to filter out.  Make
+* sure to not disturbe the original array as we need it to write our final output. A
+* secondary feature will to unmask the upper two bits that are always set to high in 
+* the original samples
+*/
+void parseInputSamplesArray(uint16_t * input, long size, long numberOfChannels, 
+							long inputChannel, uint16_t * output)
+{
+	//Remember we pull out every numberOfChannel'th sample
+	for (int i = 0; i < (size/numberOfChannels); ++i)
+	{
+		output[i] = input[i * numberOfChannels];
+		output[i] = output[i] & 0x3FFF;
+	}
+}
+
+
+/*
+*   Our Samples need to be shifted around the zero point. The input is 14 bit samples.  The 
+*   sample value is 16 bits and the samples have the upper two bits set.  Need to unmask 
+*   those
+*/
+void shiftSamplesAroundZero(double * input, long size, double shiftValue)
+{
+
+	for (int i = 0; i < SAMPLES; ++i)
+	{
+		//input[i] &= 0x3F;
+		input[i] = input[i] - shiftValue;
+
+
 	}
 
 }
 
-/*
-* Parse the input array and try to get the samples that we need to filter out.  Make
-* sure to not disturbe the original array as we need it to write our final output.
+/**
+* Convert a dobule array back to an integer array
 */
-bool parseInputSamplesArray(uint16_t * input, uint16_t size, uint16_t numberOfChannels, 
-							uint16_t inputChannel, uint16_t * output)
+void convertAndShiftOutput(double *input, uint16_t *output, int length, double shiftValue)
 {
+	int i;
 
+	//The largest value I can support is 8191
+	//The smallest value I can support is 8191
+	for (i = 0; i < length; i++) {
 
+		if (input[i] > shiftValue)
+		{
+			input[i] = shiftValue;
+		}
+		else if (input[i] < (shiftValue * -1))
+		{
+			input[i] = shiftValue * -1;
+		}
+		//shift back up now;
+		input[i] = input[i] + 8191.0;
 
+		// convert back to int.
+		output[i] = (int16_t)input[i];
 
-
-	return true;
+		//Also put back the upper two bits
+		output[i] = output[i] | 0xC000;
+	}
 
 }
+
+
 
 
 /*
@@ -75,22 +116,27 @@ int main(int argc, char * argv[])
 
 	errno_t err;
 	int fileSize;
+	long numberOfChannels = 0;
+	long inputChannel = 0;
 	
 
 
 	if (argc != 6)
 	{
 		perror("Wrong number of arguments for this program");
+		return 1;
 	}
 	//Get the arguments
 	printf("%s\n", argv[2]);
 	std::string inputFileName = argv[2];
-
+	char *p;
+	numberOfChannels = strtol(argv[3], &p, 10);
+	inputChannel = strtol(argv[4], &p, 10);
 
 
 	// open the input waveform file
 	//This file has to reside within the project directory.
-	err = fopen_s(&sampleFid, "inputFileName", "rb");
+	err = fopen_s(&sampleFid, inputFileName.c_str(), "rb");
 	if (err != 0)
 	{
 		perror("File not opened");
@@ -117,39 +163,32 @@ int main(int argc, char * argv[])
     //size_t fread(void * ptr, size_t size, size_t count, FILE * stream);
 	//Reads an array of count elements, each one with a size of size bytes, from the 
 	//stream and stores them in the block of memory specified by ptr.
-	fileSize = fread(input, sizeof(uint16_t), SAMPLES, sampleFid);
+	fileSize = fread(allChannelInput, sizeof(uint16_t), (SAMPLES * numberOfChannels), sampleFid);
+
+	//Step 1 - pull out the original sample data.
+	parseInputSamplesArray(allChannelInput, (SAMPLES * numberOfChannels), numberOfChannels, inputChannel, sampleInput);
+
 	
-	//if(!(parseInputSamplesArray(uint16_t * input, uint16_t size, uint16_t numberOfChannels,
-//		uint16_t inputChannel, uint16_t * output)
+	//Step 2 - convert to doubles
+	intToDouble(sampleInput, doubleInput, SAMPLES);
+
+	//Step 3 - Move the data around the 0 point.  8191 is 0x3FFF/2  (14 bit adc vals)
+	shiftSamplesAroundZero(doubleInput, SAMPLES, 8191);
+
+
+	// perform the filtering
+    //		firFloat(coeffs, floatInput, floatOutput, size,	FILTER_LEN);
+	// convert to ints
 
 
 
-//
-//	sampleFid = fopen("sample.bin", "rb");
-	//if (sampleFid == 0) {
-//		std::cout << "couldn't open input.pcm" << std::endl;
-		//return 1;
-	//}
+	// Use FloatInput until I put in the FIR code.
+	//doubleToInt(doubleOutput, output, fileSize);
+	convertAndShiftOutput(doubleInput, output, SAMPLES, 8191);
 
-
-
-
-	// process all of the samples
-	//do {
-	    // read samples from file
-
-		// convert to doubles
-		intToFloat(input, floatInput, fileSize);
-		// perform the filtering
-//		firFloat(coeffs, floatInput, floatOutput, size,	FILTER_LEN);
-		// convert to ints
-
-
-
-		// Use FloatInput until I put in the FIR code.
-		floatToInt(floatInput, output, fileSize);
-		// write samples to file
-		fwrite(output, sizeof(int16_t), fileSize, sampleOutFid); 
+	
+	// write samples to file
+		//fwrite(output, sizeof(int16_t), fileSize, sampleOutFid); 
 		//
 
 		//This portion is test code to output our data into a readable format.
@@ -157,15 +196,24 @@ int main(int argc, char * argv[])
 		//The input file is currently in 4 Channels (ie: 4 columns of data).  Only the first 
 		//channel is valid.  I am simply putting it back into the 4 columns.  This is all
 		//hardcoded for now but the number of channels will ultimately be passed in.
-		for (int i = 0; i < 250; ++i)
-		{ 
-			for (int y = 0; y < 4; ++y)
-			{
-				fprintf(sampleOutTextFid, "%hu,", output[(i*4)+y]);
+		//for (int i = 0; i < 250; ++i)
+		//{ 
+//			for (int y = 0; y < 4; ++y)
+			//{
+//				fprintf(sampleOutTextFid, "%hu,", sampleInput[(i * 4) + y]);
 			
-			}
-			fprintf(sampleOutTextFid, "\n");
+//			}
+			//fprintf(sampleOutTextFid, "\n");
+//		}
+
+		for (int i = 0; i < 250; ++i)
+		{
+			fprintf(sampleOutTextFid, "%hu,%hu\n", sampleInput[i], output[i]);
 		}
+
+
+
+
 //	} while (size != 0);
 
 		fclose(sampleFid);
